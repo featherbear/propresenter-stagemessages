09adf001a6c87ca5677ad49fd7609bf71ec9f4e4
featherbear <andrew.j.wong@outlook.com>
2018-08-05 12:24:04 +1000

    Current message + blink ESC key clears the input box Moved options button to the top right

--- site.js
+++ site.js
@@ -24,19 +24,29 @@ function authFailed() {
 
 elem("message").addEventListener("keyup", function(event) {
     event.preventDefault();
-    if (event.keyCode === 13) {
-        elem("message").value && elem("send").click();
-        elem("message").value = "";
+    switch (event.keyCode) {
+        case 13:
+            elem("message").value.trim() && elem("send").click();
+        case 27:
+            elem("message").value = "";
+            break;
     }
 });
 elem("send").addEventListener("click", function() {
     let message = elem("message").value.trim();
     stageMessageSend(message);
-    elem("currentMessage").value = message;
+
+    var oldMessage = elem("currentMessage");
+    var newMessage = oldMessage.cloneNode(true);
+    newMessage.innerText = message;
+    newMessage.className = "blink";
+    oldMessage.parentNode.replaceChild(newMessage, oldMessage);
+
 })
 elem("clear").addEventListener("click", function() {
     stageMessageHide();
-    elem("currentMessage").value = "";
+    elem("currentMessage").innerText = " ";
+    elem("currentMessage").className = "";
 })
 
 function showPreferences() {
@@ -47,7 +57,7 @@ function showPreferences() {
 }
 elem("pref_open").addEventListener("click", showPreferences);
 
-elem("pref_close").addEventListener("click", function() {
+elem("pref_save").addEventListener("click", function() {
     localStorage.setItem("host", elem("pref_addr").value || default_host);
     localStorage.setItem("port", elem("pref_port").value || default_port);
     localStorage.setItem("pass", elem("pref_pass").value || default_pass);
