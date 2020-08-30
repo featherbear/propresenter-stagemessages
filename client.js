9c3fcefcfb857985bbc4f6333df5720e8a8abf05
Andrew Wong <andrew.j.wong@outlook.com>
2020-08-30 23:50:19 +1000

    Format to JavaScript Standard

--- client.js
+++ client.js
@@ -1,87 +1,87 @@
 // C:\Program Files (x86)\Renewed Vision\ProPresenter 6\ProPresenter.UI.Plugin.dll
 // ProPresenter.UI.Plugin.ProNetwork.RVProRemoteWebSocketServiceHandler
 
-const debug = false;
-const default_host = 'localhost';
-const default_port = '50001';
-const default_pass = 'control';
+const debug = false
+const default_host = 'localhost'
+const default_port = '50001'
+const default_pass = 'control'
 
-let socket;
-let cb_connectSuccess;
-let cb_connectFail;
-let cb_authSuccess;
-let cb_authFail;
+let socket
+let cb_connectSuccess
+let cb_connectFail
+let cb_authSuccess
+let cb_authFail
 
-function err(s) {
-    debug && console.error(`Error: ${s}`);
-    return false;
+function err (s) {
+  debug && console.error(`Error: ${s}`)
+  return false
 }
 
-function check_socket() {
-    if (!socket) return err('SOCKET NOT CONNECTED');
-    if (socket.readyState != 1) return err('SOCKET NOT READY');
-    return true;
+function check_socket () {
+  if (!socket) return err('SOCKET NOT CONNECTED')
+  if (socket.readyState != 1) return err('SOCKET NOT READY')
+  return true
 }
 
-function connect(obj) {
-    obj.connectSuccess && (cb_connectSuccess = obj.connectSuccess);
-    obj.connectFail && (cb_connectFail = obj.connectFail);
-    obj.authSuccess && (cb_authSuccess = obj.authSuccess);
-    obj.authFail && (cb_authFail = obj.authFail);
+function connect (obj) {
+  obj.connectSuccess && (cb_connectSuccess = obj.connectSuccess)
+  obj.connectFail && (cb_connectFail = obj.connectFail)
+  obj.authSuccess && (cb_authSuccess = obj.authSuccess)
+  obj.authFail && (cb_authFail = obj.authFail)
 
-    var host = localStorage.getItem("host") || default_host;
-    var port = localStorage.getItem("port") || default_port;
-    debug && console.log(`Connecting to ws://${host}:${port}/remote`);
-    socket = new WebSocket(`ws://${host}:${port}/remote`);
-    // Above operation is non-blocking. Have to wait for the socket to connect before we authenticate()
-    socket.onclose = socket.onerror = cb_connectFail;
-    socket.onopen = function() {
-        cb_connectSuccess && cb_connectSuccess();
-        debug && console.log("Connection success, authenticating...");
-        authenticate()
-    };
-    listen();
+  var host = localStorage.getItem('host') || default_host
+  var port = localStorage.getItem('port') || default_port
+  debug && console.log(`Connecting to ws://${host}:${port}/remote`)
+  socket = new WebSocket(`ws://${host}:${port}/remote`)
+  // Above operation is non-blocking. Have to wait for the socket to connect before we authenticate()
+  socket.onclose = socket.onerror = cb_connectFail
+  socket.onopen = function () {
+    cb_connectSuccess && cb_connectSuccess()
+    debug && console.log('Connection success, authenticating...')
+    authenticate()
+  }
+  listen()
 }
 
-function authenticate() {
-    emit({
-        action: 'authenticate',
-        protocol: '600',
-        password: localStorage.getItem("pass") || default_pass
-    })
+function authenticate () {
+  emit({
+    action: 'authenticate',
+    protocol: '600',
+    password: localStorage.getItem('pass') || default_pass
+  })
 }
 
-function listen() {
-    socket.onmessage = function(event) {
-        var msg = JSON.parse(event.data);
+function listen () {
+  socket.onmessage = function (event) {
+    var msg = JSON.parse(event.data)
 
-        switch (msg.action) {
-            case "authenticate":
-                debug && console.log("Authentication " + (msg.authenticated ? "success" : "failed"));
-                msg.authenticated ? (cb_authSuccess && cb_authSuccess()) : (cb_authFail && cb_authFail());
-                break;
-            default:
-                console.log(msg);
-                break;
-        }
+    switch (msg.action) {
+      case 'authenticate':
+        debug && console.log('Authentication ' + (msg.authenticated ? 'success' : 'failed'))
+        msg.authenticated ? (cb_authSuccess && cb_authSuccess()) : (cb_authFail && cb_authFail())
+        break
+      default:
+        console.log(msg)
+        break
     }
+  }
 }
 
-function stageMessageSend(msg) {
-    emit({
-        action: 'stageDisplaySendMessage',
-        stageDisplayMessage: msg
-    })
+function stageMessageSend (msg) {
+  emit({
+    action: 'stageDisplaySendMessage',
+    stageDisplayMessage: msg
+  })
 }
 
-function stageMessageHide() {
-    emit({
-        action: 'stageDisplayHideMessage'
-    })
+function stageMessageHide () {
+  emit({
+    action: 'stageDisplayHideMessage'
+  })
 }
 
-function emit(obj) {
-    var json = JSON.stringify(obj);
-    if (check_socket()) socket.send(json);
-    else return err('SOCKET EMIT FAILED');
+function emit (obj) {
+  var json = JSON.stringify(obj)
+  if (check_socket()) socket.send(json)
+  else return err('SOCKET EMIT FAILED')
 }
--- site.js
+++ site.js
@@ -1,105 +1,102 @@
 // load client.js first!
 const elem = id => document.getElementById(id);
 
-(status_disconnected = () => elem("status").className = (elem("status").innerText = "Disconnected").toLowerCase())();
-status_badAuth = () => (elem("status").className = "disconnected") && (elem("status").innerText = "Bad password... retrying in 3 seconds");
-status_connecting = () => elem("status").className = (elem("status").innerText = "Connecting").toLowerCase();
-status_connected = () => elem("status").className = (elem("status").innerText = "Connected").toLowerCase();
+(status_disconnected = () => elem('status').className = (elem('status').innerText = 'Disconnected').toLowerCase())()
+status_badAuth = () => (elem('status').className = 'disconnected') && (elem('status').innerText = 'Bad password... retrying in 3 seconds')
+status_connecting = () => elem('status').className = (elem('status').innerText = 'Connecting').toLowerCase()
+status_connected = () => elem('status').className = (elem('status').innerText = 'Connected').toLowerCase()
 
-let cb = {
+const cb = {
   connectFail: function () {
-    status_disconnected();
-    connectFail();
+    status_disconnected()
+    connectFail()
   },
   authSuccess: function () {
-    clearTimeout(connectLoop);
-    status_connected();
+    clearTimeout(connectLoop)
+    status_connected()
   },
   authFail: function () {
-    status_badAuth();
-    connectFail();
-  },
+    status_badAuth()
+    connectFail()
+  }
 }
 
-let connectLoop;
-let shakeLoop;
+let connectLoop
+let shakeLoop
 
-function connectFail() {
-    clearTimeout(connectLoop);
-    connectLoop = setTimeout(function() {
-        status_connecting();
-        connect(cb);
-    }, 3000);
+function connectFail () {
+  clearTimeout(connectLoop)
+  connectLoop = setTimeout(function () {
+    status_connecting()
+    connect(cb)
+  }, 3000)
 }
 
-elem("message").addEventListener("keyup", function(event) {
-    event.preventDefault();
-    switch (event.keyCode) {
-        case 13:
-            elem("message").value.trim() && elem("send").click();
-        case 27:
-            elem("message").value = "";
-            break;
-    }
-});
-elem("send").addEventListener("click", function() {
-    let message;
-    if (!(message = elem("message").value.trim())) return false;
-    stageMessageSend(message);
-    
-    clearTimeout(shakeLoop)
-    elem("send").className="shake";
-    shakeLoop = setTimeout(()=>elem("send").className="",200);
-    
-
-    var oldMessage = elem("currentMessage");
-    var newMessage = oldMessage.cloneNode(true);
-    newMessage.innerText = message;
-    newMessage.className = "blink";
-    oldMessage.parentNode.replaceChild(newMessage, oldMessage);
-
+elem('message').addEventListener('keyup', function (event) {
+  event.preventDefault()
+  switch (event.keyCode) {
+    case 13:
+      elem('message').value.trim() && elem('send').click()
+    case 27:
+      elem('message').value = ''
+      break
+  }
 })
-elem("clear").addEventListener("click", function() {
-    stageMessageHide();
-    elem("currentMessage").innerText = " ";
-    elem("currentMessage").className = "";
+elem('send').addEventListener('click', function () {
+  let message
+  if (!(message = elem('message').value.trim())) return false
+  stageMessageSend(message)
+
+  clearTimeout(shakeLoop)
+  elem('send').className = 'shake'
+  shakeLoop = setTimeout(() => elem('send').className = '', 200)
+
+  var oldMessage = elem('currentMessage')
+  var newMessage = oldMessage.cloneNode(true)
+  newMessage.innerText = message
+  newMessage.className = 'blink'
+  oldMessage.parentNode.replaceChild(newMessage, oldMessage)
+})
+elem('clear').addEventListener('click', function () {
+  stageMessageHide()
+  elem('currentMessage').innerText = ' '
+  elem('currentMessage').className = ''
 })
 
-function showPreferences() {
-    elem("preferences").showModal();
-    elem("pref_addr").value = localStorage.getItem("host") || default_host;
-    elem("pref_port").value = localStorage.getItem("port") || default_port;
-    elem("pref_pass").value = localStorage.getItem("pass") || default_pass;
+function showPreferences () {
+  elem('preferences').showModal()
+  elem('pref_addr').value = localStorage.getItem('host') || default_host
+  elem('pref_port').value = localStorage.getItem('port') || default_port
+  elem('pref_pass').value = localStorage.getItem('pass') || default_pass
 }
-elem("pref_open").addEventListener("click", showPreferences);
+elem('pref_open').addEventListener('click', showPreferences)
 
-
-if (typeof elem("preferences").showModal !== "function") {
-    elem("preferences").showModal = function() {
-        this.setAttribute('open', true)
-    }
+if (typeof elem('preferences').showModal !== 'function') {
+  elem('preferences').showModal = function () {
+    this.setAttribute('open', true)
+  }
 }
 
-if (typeof elem("preferences").close !== "function") {
-    elem("preferences").close = function() {
-        this.removeAttribute('open')
-    }
+if (typeof elem('preferences').close !== 'function') {
+  elem('preferences').close = function () {
+    this.removeAttribute('open')
+  }
 }
 
 document.querySelector('.dialogBackground').addEventListener('click', () => {
-    elem("preferences").close()
+  elem('preferences').close()
 })
 
-elem("pref_save").addEventListener("click", function() {
-    localStorage.setItem("host", elem("pref_addr").value || default_host);
-    localStorage.setItem("port", elem("pref_port").value || default_port);
-    localStorage.setItem("pass", elem("pref_pass").value || default_pass);
-    socket.close()
-    status_disconnected();
-    connect(cb);
-    status_connecting();
-    elem("preferences").close();
+elem('pref_save').addEventListener('click', function () {
+  localStorage.setItem('host', elem('pref_addr').value || default_host)
+  localStorage.setItem('port', elem('pref_port').value || default_port)
+  localStorage.setItem('pass', elem('pref_pass').value || default_pass)
+  socket.close()
+  status_disconnected()
+  connect(cb)
+  status_connecting()
+  elem('preferences').close()
 })
 
-status_connecting();
-connect(cb);
+status_connecting()
+connect(cb)
